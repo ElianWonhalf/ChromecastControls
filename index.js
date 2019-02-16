3cfbbf34b574acf1e12034b658eb80e330a48709
Elian Wonhalf <elianwonhalf@gmail.com>
2019-02-16 14:46:13 -0500

    Added more time controls

--- index.js
+++ index.js
@@ -30,12 +30,52 @@ const controlVideo = async () => {
                     value: 'stop'
                 },
                 {
+                    name: '+30 minutes',
+                    value: 'seek30m'
+                },
+                {
+                    name: '+10 minutes',
+                    value: 'seek10m'
+                },
+                {
+                    name: '+5 minutes',
+                    value: 'seek5m'
+                },
+                {
+                    name: '+1 minute',
+                    value: 'seek1m'
+                },
+                {
+                    name: '+30 seconds',
+                    value: 'seek30s'
+                },
+                {
                     name: '+10 seconds',
-                    value: 'seek'
+                    value: 'seek10s'
+                },
+                {
+                    name: '-30 minutes',
+                    value: 'rewind30m'
+                },
+                {
+                    name: '-10 minutes',
+                    value: 'rewind10m'
+                },
+                {
+                    name: '-5 minutes',
+                    value: 'rewind5m'
+                },
+                {
+                    name: '-1 minute',
+                    value: 'rewind1m'
+                },
+                {
+                    name: '-30 seconds',
+                    value: 'rewind30s'
                 },
                 {
                     name: '-10 seconds',
-                    value: 'rewind'
+                    value: 'rewind10s'
                 }
             ]
         }
@@ -52,11 +92,51 @@ const controlVideo = async () => {
             process.exit();
             break;
 
-        case 'seek':
+        case 'seek30m':
+            device.seek(30 * 60);
+            break;
+
+        case 'seek10m':
+            device.seek(10 * 60);
+            break;
+
+        case 'seek5m':
+            device.seek(5 * 60);
+            break;
+
+        case 'seek1m':
+            device.seek(60);
+            break;
+
+        case 'seek30s':
             device.seek(10);
             break;
 
-        case 'rewind':
+        case 'seek10s':
+            device.seek(10);
+            break;
+
+        case 'rewind30m':
+            device.seek(-30 * 60);
+            break;
+
+        case 'rewind10m':
+            device.seek(-10 * 60);
+            break;
+
+        case 'rewind5m':
+            device.seek(-5 * 60);
+            break;
+
+        case 'rewind1m':
+            device.seek(-60);
+            break;
+
+        case 'rewind30s':
+            device.seek(-30);
+            break;
+
+        case 'rewind10s':
             device.seek(-10);
             break;
     }
